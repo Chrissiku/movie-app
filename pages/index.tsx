3ace6a294db49d8446649c574521b276183aa758
Chris Siku <chrissiku5@gmail.com>
2023-02-04 15:22:06 +0200

    consume API inside the `getServerSideProps` function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import { Inter } from "@next/font/google";
 import SubHomePage from "@/components/pages/Home";
-
 const inter = Inter({ subsets: ["latin"] });
 
-const Home = () => {
+type Data = {
+  data: any;
+};
+
+const Home = ({ data }: Data) => {
+  console.log({ movieData: data });
   return (
     <>
       <div>
@@ -11,6 +15,22 @@ const Home = () => {
       </div>
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  try {
+    const response = await fetch(
+      "http://www.omdbapi.com/?apikey=208c5684&s=Batman"
+    )
+      .then((result) => result.json())
+      .then((res) => res);
+
+    return {
+      props: { data: response },
+    };
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default Home;
