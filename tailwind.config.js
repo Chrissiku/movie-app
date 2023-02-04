c1cc7381659343f7bb22f1245f238d1736a4e0c7
Chris Siku <chrissiku5@gmail.com>
2023-02-04 13:17:58 +0200

    Configure Twailwinf css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,29 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#d81159",
+        secondary: "#2f3e46",
+        dark: "#000814",
+        white: "#ffffff",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
   },
   plugins: [],
-}
+};
